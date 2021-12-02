7bbcaafe6e7caf6b15352c96d8db3294234108cb
ArtisanGray <1697@holbertonschool.com>
2021-12-02 03:49:01 +0000

    created tasks 8-12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+	public static void PrintSorted(Dictionary<string, string> myDict)
+	{
+		List<string> sortedList = myDict.Keys.ToList();
+		sortedList.Sort();
+
+		foreach (string key in sortedList)
+		{
+			Console.WriteLine("{0}: {1}", key, myDict[key]);
+		}
+	}
+}
