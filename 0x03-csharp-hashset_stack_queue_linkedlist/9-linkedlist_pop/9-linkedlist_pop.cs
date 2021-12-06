82ed850d2ff0045c85574f9f9ea9e68a4e8ffa25
ArtisanGray <1697@holbertonschool.com>
2021-12-06 03:26:02 +0000

    second deadline fix on 9-pop, fixed empty list case

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -7,6 +7,8 @@ class LList
 	public static int Pop(LinkedList<int> myLList)
 	{
 		int nodeValue = myLList.First.Value;
+		if (myLList.Count == 0)
+			return (0);
 		myLList.Remove(myLList.First);
 		return (nodeValue);
 	}
