13257697da0da5016a4c98015adcc573a993c76a
ArtisanGray <1697@holbertonschool.com>
2021-12-15 02:57:58 +0000

    still failing a check

--- 0x06-csharp-classes/0-enemy/0-enemy.cs
+++ 0x06-csharp-classes/0-enemy/0-enemy.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Enemies
 {
-	/// <summary> An empty Zombie class </summary>
+	/// <summary> 
+	/// An empty Zombie class 
+	/// </summary>
 	public class Zombie
 	{
 	}
